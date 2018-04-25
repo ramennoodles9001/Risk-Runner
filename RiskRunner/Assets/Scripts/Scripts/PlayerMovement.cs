13b4b09f5fc312055803ea3b64853ddb7bcae88a
ramennoodles9001 <31226419+ramennoodles9001@users.noreply.github.com>
2018-04-25 18:23:48 -0400

    Push to desktop scripts

---
+++ RiskRunner/Assets/Scripts/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public bool allowInput=true;
+	private CharacterController characterController;
+	private Vector3 moveDirection;
+	
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 0;
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(allowInput){
+			moveDirection = new Vector3(Input.GetAxis("Horizontal")*moveSpeed,0,Input.GetAxis("Vertical")*moveSpeed);
+		}
+		moveDirection = transform.TransformDirection(transform.forward);
+		characterController.Move(moveDirection);
+	}
+}
